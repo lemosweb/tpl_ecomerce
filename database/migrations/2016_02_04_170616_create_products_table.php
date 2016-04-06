4edef81bebe1aaded5a578ed73dec0d4ae33d96e
Doniex Lemavorum <doniexlemavorum@gmail.com>
2016-04-06 12:05:58 -0300

    databa

--- database/migrations/2016_02_04_170616_create_products_table.php
+++ database/migrations/2016_02_04_170616_create_products_table.php
@@ -12,7 +12,7 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        //DB::statement('set foreign_key_checks = 0;');
+        DB::statement('set foreign_key_checks = 0;');
         Schema::create('products', function (Blueprint $table) {
 
             $table->increments('id');
@@ -26,7 +26,7 @@ class CreateProductsTable extends Migration
             $table->timestamps();
 
         });
-        //DB::statement('set foreign_key_checks = 1;');
+        DB::statement('set foreign_key_checks = 1;');
     }
 
     /**
